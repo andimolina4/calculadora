a6975f0decd4811dd6d31abdf4fc0bcee12f2170
Andrea <andimolina629@gmail.com>
2023-02-08 19:26:07 -0300

    1er commit- creación componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      CalculadoraComponent
+   ],
   imports: [
     BrowserModule
   ],
